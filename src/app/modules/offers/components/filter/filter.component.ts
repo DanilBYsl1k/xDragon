0790aa3d555bb5e36f825d691268bdc9cf237c43
Ivan Rudnichenko <rudnicenko.ivan@gmail.com>
2023-02-25 20:18:14 +0200

    made basics template for offers page

--- src/app/modules/offers/components/filter/filter.component.ts
+++ src/app/modules/offers/components/filter/filter.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
-
+  items = ['Sort by language', 'Sory by experiance', 'Sort by location', 'Sort by company'];
+  expandedIndex = 0;
 }
--- src/app/modules/offers/components/offers-list/offers-list.component.ts
+++ src/app/modules/offers/components/offers-list/offers-list.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./offers-list.component.scss']
 })
 export class OffersListComponent {
-  offers: number[] = [1,1,1,1,11,1,11,1,1,1,1,1,1,1,1]
+  offers: any[] = [
+    {
+      title: ".NET Infrastructure Developer",
+      description: "The .NET infrastructure developer is responsible for the development and maintenance of several legacy C#/.NET applications used by the company. The majority of these are back-end infrastructure projects. The .NET infrastructure developer is responsible for the development and maintenance of several legacy C#/.NET applications used by the company. The majority of these are back-end infrastructure projects. The .NET infrastructure developer is responsible for the development and maintenance of several legacy C#/.NET applications used by the company. The majority of these are back-end infrastructure projects. The .NET infrastructure developer is responsible for the development and maintenance of several legacy C#/.NET applications used by the company. The majority of these are back-end infrastructure projects",
+      benefits: ["Remote", "Medical insurance", "High salary", "Remote", "Medical insurance", "High salary"],
+      company: {
+        logo: "https://p.djinni.co/42/17074426e5f3bbad15ec7c6e08dbc4/Luxoft_Purple_RGB_400.png",
+        name: "Luxoft"
+      }
+    },
+    {
+      title: ".NET Infrastructure Developer",
+      description: "The .NET infrastructure developer is responsible for the development and maintenance of several legacy C#/.NET applications used by the company. The majority of these are back-end infrastructure projects",
+      benefits: ["Remote", "Medical insurance", "High salary"],
+      company: {
+        logo: "https://p.djinni.co/42/17074426e5f3bbad15ec7c6e08dbc4/Luxoft_Purple_RGB_400.png",
+        name: "Luxoft"
+      }
+    },
+    {
+      title: ".NET Infrastructure Developer",
+      description: "The .NET infrastructure developer is responsible for the development and maintenance of several legacy C#/.NET applications used by the company. The majority of these are back-end infrastructure projects",
+      benefits: ["Remote", "Medical insurance", "High salary"],
+      company: {
+        logo: "https://p.djinni.co/42/17074426e5f3bbad15ec7c6e08dbc4/Luxoft_Purple_RGB_400.png",
+        name: "Luxoft"
+      }
+    },
+    {
+      title: ".NET Infrastructure Developer",
+      description: "The .NET infrastructure developer is responsible for the development and maintenance of several legacy C#/.NET applications used by the company. The majority of these are back-end infrastructure projects",
+      benefits: ["Remote", "Medical insurance", "High salary"],
+      company: {
+        logo: "https://p.djinni.co/42/17074426e5f3bbad15ec7c6e08dbc4/Luxoft_Purple_RGB_400.png",
+        name: "Luxoft"
+      }
+    },
+    {
+      title: ".NET Infrastructure Developer",
+      description: "The .NET infrastructure developer is responsible for the development and maintenance of several legacy C#/.NET applications used by the company. The majority of these are back-end infrastructure projects",
+      benefits: ["Remote", "Medical insurance", "High salary"],
+      company: {
+        logo: "https://p.djinni.co/42/17074426e5f3bbad15ec7c6e08dbc4/Luxoft_Purple_RGB_400.png",
+        name: "Luxoft"
+      }
+    }
+  ]
+
+  formateDescription(desc: string){
+    if (desc.length > 250) return desc.substring(0,250) + `...`
+
+    return desc
+  }
 }
